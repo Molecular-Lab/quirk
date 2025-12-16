8ce681a7d0d2d22abcbbd1c90e4ebb7372203aef
FOwen123 <filbertowen.s@gmail.com>
2025-12-16 23:57:35 +0800

    merge strategies page to productconfigpage

--- apps/whitelabel-web/src/routes/dashboard/strategies.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { MyStrategiesPage } from '../../feature/dashboard/MyStrategiesPage'
-
-export const Route = createFileRoute('/dashboard/strategies')({
-  component: MyStrategiesPage,
-})
